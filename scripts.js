f418d8a4ab5a618302ecfeeec1c6e5b3bdd52d2e
Ebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2022-02-19 10:22:11 -0800

    test

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-ebukasite() {
+function ebukasite () {
     window.location.assign("ebuka.html");
+
 }
+function brysonsite () {
+    window.location.assign("bryson.html");
+
+}
+function tamarasite () {
+    window.location.assign("tamara.html");
+
+}
+
